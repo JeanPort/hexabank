70b4ad226eb015ed83a539f2e03f58a33d543e30
jeanport <pierreanatc522@gmail.com>
2025-01-10 19:21:24 -0300

    Iniciando projeto

---
+++ src/main/java/com/jean/hexabank/HexabankApplication.java
@@ -0,0 +1,13 @@
+package com.jean.hexabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/hexabank/HexabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.hexabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
